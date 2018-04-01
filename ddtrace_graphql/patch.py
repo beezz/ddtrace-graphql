2696f131792badbe03859851cd1aa98bbfae0160
Michal Kuffa <michal@bynder.com>
2018-04-01 02:36:48 +0200

    Fix compatibility with graphql-core==1.1.0

--- ddtrace_graphql/patch.py
+++ ddtrace_graphql/patch.py
@@ -58,9 +58,37 @@ def patch(span_kwargs=None):
 
     wrapt.wrap_function_wrapper(graphql, 'graphql', wrapper)
 
+
 def unpatch():
     logger.debug('Unpatching `graphql.graphql` function.')
     unwrap(graphql, 'graphql')
+
+
+def _is_server_error(result):
+    return bool(
+        (
+            result.errors
+            and not result.invalid
+        )
+        or
+        (
+            result.errors
+            and result.invalid
+            and len(result.errors) == 1
+            and not isinstance(result.errors[0], GraphQLError)
+        )
+    )
+
+
+def _format_errors(errors):
+    return json.dumps(
+        [
+            # fix for graphql-core==1.x
+            format_error(err) if hasattr(err, 'message') else str(err)
+            for err in errors
+        ],
+        indent=2,
+    )
 
 
 def _resolve_query_res(query):
@@ -111,28 +139,17 @@ def _traced_graphql(func, args, kwargs, span_kwargs=None):
         finally:
             # `span.error` must be integer
             span.error = int(result is None)
+
             if result is not None:
+
                 result.errors and span.set_tag(
-                    ERRORS,
-                    json.dumps(
-                        [format_error(err) for err in result.errors],
-                        indent=4,
-                    ),
-                )
+                    ERRORS, _format_errors(result.errors))
+                span.error = int(_is_server_error(result))
+
                 span.set_metric(INVALID, int(result.invalid))
                 span.set_metric(DATA_EMPTY, int(result.data is None))
-                if result.errors and not result.invalid:
-                    span.error = 1
-                elif (
-                    result.errors
-                    and result.invalid
-                    and len(result.errors) == 1
-                    and not isinstance(result.errors[0], GraphQLError)
-                ):
-                    # based on execute_graphql implementation
-                    # graphql/graphql.py#L41
-                    span.error = 1
 
 
 def traced_graphql(*args, span_kwargs=None, **kwargs):
     return _traced_graphql(_graphql, args, kwargs, span_kwargs=span_kwargs)
+

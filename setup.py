e2022f392e98dfa84129b6443e81208477cd1a0e
Michal Kuffa <beezz@users.noreply.github.com>
2019-05-15 21:51:43 +0200

    Update pkg (#82)
    - remove requirements.txt prefer only setup.py
    - do not use requirements.txt in tox
    - depend on wrapt
    - update travis config ~ drop 3.6, add 3.7

--- setup.py
+++ setup.py
@@ -4,40 +4,31 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, 'VERSION'), encoding='utf-8') as f:
+with open(path.join(here, "VERSION"), encoding="utf-8") as f:
     version = f.read()
 
 
 setup(
-    name='ddtrace-graphql',  # Required
+    name="ddtrace-graphql",  # Required
     version=version,  # Required
-    description='Python library for tracing graphql calls with Datadog',
+    description="Python library for tracing graphql calls with Datadog",
     long_description=long_description,  # Optional
-    url='https://github.com/beezz/ddtrace-graphql',  # Optional
-    author='Michal Kuffa',  # Optional
-    author_email='michal@bynder.com',  # Optional
+    url="https://github.com/beezz/ddtrace-graphql",  # Optional
+    author="Michal Kuffa",  # Optional
+    author_email="michal@bynder.com",  # Optional
     classifiers=[  # Optional
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Debuggers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Debuggers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='tracing datadog graphql graphene',
-    packages=find_packages(exclude=['tests']),  # Required
-    install_requires=[
-        'ddtrace',
-        'graphql-core',
-    ],
-    extras_require={
-        'test': [
-            'tox',
-            'pytest',
-            'pytest-cov',
-        ],
-    }
+    keywords="tracing datadog graphql graphene",
+    packages=find_packages(exclude=["tests"]),  # Required
+    install_requires=["ddtrace", "graphql-core", "wrapt"],
+    extras_require={"test": ["tox", "pytest", "pytest-cov"]},
 )

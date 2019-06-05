14123ae8f36a7d9f9a1027db4870ed0c09e11269
Michal Kuffa <michal.kuffa@radarservices.com>
2019-06-05 21:44:07 +0200

    Patch also `execute_and_validate` to suport server-core

--- ddtrace_graphql/patch.py
+++ ddtrace_graphql/patch.py
@@ -8,6 +8,7 @@ import logging
 import os
 
 import graphql
+import graphql.backend.core
 import wrapt
 from ddtrace.util import unwrap
 
@@ -20,19 +21,27 @@ def patch(span_kwargs=None, span_callback=None, ignore_exceptions=()):
     """
     Monkeypatches graphql-core library to trace graphql calls execution.
     """
-    logger.debug('Patching `graphql.graphql` function.')
 
     def wrapper(func, _, args, kwargs):
         return traced_graphql_wrapped(
-            func, args, kwargs,
+            func,
+            args,
+            kwargs,
             span_kwargs=span_kwargs,
             span_callback=span_callback,
-            ignore_exceptions=ignore_exceptions
+            ignore_exceptions=ignore_exceptions,
         )
 
-    wrapt.wrap_function_wrapper(graphql, 'graphql', wrapper)
+    logger.debug("Patching `graphql.graphql` function.")
+
+    wrapt.wrap_function_wrapper(graphql, "graphql", wrapper)
+
+    logger.debug("Patching `graphql.backend.core.execute_and_validate` function.")
+
+    wrapt.wrap_function_wrapper(graphql.backend.core, "execute_and_validate", wrapper)
 
 
 def unpatch():
-    logger.debug('Unpatching `graphql.graphql` function.')
-    unwrap(graphql, 'graphql')
+    logger.debug("Unpatching `graphql.graphql` function.")
+    unwrap(graphql, "graphql")
+    unwrap(graphql.backend.core, "execute_and_validate")
